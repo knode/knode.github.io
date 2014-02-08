3e97b8f9cea24177dbc58e98e3a91b84c5378824
Tracy Abrahms <tracyabrahms@gmail.com>
2014-02-08 15:22:32 -0800

    add speaker profile object

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-var fs = require('fs');
-var handlebars = require('handlebars');
-var _ = require('underscore');
+var fs = require('fs')
+  , handlebars = require('handlebars')
+  , _ = require('underscore')
+  , marked = require('marked');
 
 var cities = [
     {
@@ -13,14 +14,15 @@ var cities = [
             , github: "justinabrahms"
             , preferred_audience_size: "any"
             , travel_availability: "if comped"
-        }
-        , {
+            , about_me: "I'm Justin. I've been programming for many years. I'm\nself-taught. I dig talking because it provides me an excuse to dig\ndeeper into a topic, as well as a method of share my findings with\nothers.\n\nAs for formats, I'm happy with a tour of code, if there's a\nparticular repo of mine that catches your eye, standard speaker +\nslides or round table discussion things.\n\nFor topics, I'm into a bunch of stuff, including:\n\n- static analysis\n- writing testable code\n- playing with irc bots\n- application structure and architecture\n- technical paper discussion\n- computer science (and education thereof)\n- tooling as a general concept\n"
+        }, {
             name: "Christopher Swenson"
             , location: "Portland, OR"
             , twitter: "@caswenson"
             , github: "caswenson"
             , preferred_audience_size: "large"
             , travel_availability: "local"
+            , about_me: "Nothing to see here. Move along."
         }
       ]
     }
@@ -33,14 +35,22 @@ var cities = [
         , github: "jden"
         , preferred_audience_size: "small"
         , travel_availability: "any"
+        , about_me: "Nothing to see here. Move along."
       }]
     }
 ];
 
 function get_speakers() { 
-    return {
-        cities: cities
-    };
+    return speaker_profile({cities: cities})
+
+    function speaker_profile(city_map) {
+      _.each(city_map.cities, function(city) {
+          _.each(city.speakers, function(speaker) {
+            speaker.about_me = marked(speaker.about_me)
+          })
+      })
+      return city_map
+    }
 };
 
 /**
@@ -54,21 +64,23 @@ var template_contextfn = {
 
 function compile () { 
   _.each(template_contextfn, function (ctxFn, template) {
-    var outputFile = template.replace('hbs', 'html');
-    var ctx = template_contextfn[template]();
+    var outputFile = template.replace('hbs', 'html')
+    , ctx = template_contextfn[template]()
+
     fs.readFile('templates/' + template, 'utf8', function (err, txt) {
       if (err) {
-        throw err;
+        throw err
       }
-      var output = handlebars.compile(txt)(ctx);
+      var output = handlebars.compile(txt)(ctx)
+
       fs.writeFile('output/' + outputFile, output, function (err) {
         if (err) {
-          throw err;
+          throw err
         }
         console.log("Wrote", template, 'to', outputFile);
-      });
-    });
-  });
-}
+      })
+    })
+  })
+};
 
 compile();
